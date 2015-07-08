864230fa47e13c8ed8ec3bd4de22081008b02b10
Mohammad Hossein Ghaffari <mhgh@users.noreply.github.com>
2015-07-09 01:48:59 +0430

    Update mmd.js

--- libs/mmd.js
+++ libs/mmd.js
@@ -6,6 +6,36 @@ $(document).ready(function() {
  		Preview.Update(this);
 	});          
 
+        
+
+        
+        // Actual default values
+        var md = new Remarkable({
+        html:         true,        // Enable HTML tags in source
+        xhtmlOut:     false,        // Use '/' to close single tags (<br />)
+        breaks:       false,        // Convert '\n' in paragraphs into <br>
+        langPrefix:   'language-',  // CSS language prefix for fenced blocks
+        linkify:      false,        // Autoconvert URL-like text to links
+
+        // Enable some language-neutral replacement + quotes beautification
+        typographer:  false,
+
+        // Double + single quotes replacement pairs, when typographer enabled,
+        // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+        quotes: '“”‘’',
+
+        // Highlighter function. Should return escaped HTML,
+        // or '' if the source string is not changed
+        highlight: function (/*str, lang*/) { return ''; }
+        });        
+
+// console.log(md.render('# Remarkable rulezz!'));        
+
+        $('.entry-content').each(function () {
+                this.innerHTML=md.render(this.innerHTML);
+        });
+
+
 });
 
 
